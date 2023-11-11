318d9102e794d131ba9dddbdc84318dd6ff5cc38
Alex Nohrin <beterok@inbox.ru>
2023-11-11 20:42:03 +0500

    7.3 task one

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,45 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const data = [                                                  
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const expected = [                                               
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    const result = sorting.sortByName(data);                         
+
+    expect(result).toEqual(expected);                              
   });
+
+  it("Should throw an exception when calling", () => {               
+    expect(() => sorting.sortByName()).toThrow();
+  })
+
+  it("Books names should be sorted even if names recurring", () => {
+    const data = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ];
+
+    const expected = [
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+      "Гарри Поттер",
+    ];
+
+    const result = sorting.sortByName(data);                       
+
+    expect(result).toEqual(expected);                              
+  });
+
 });
